8b2dd18997f70cb2c4940450bb4760af671edddd
Sentpiqe <sentpiqe@proton.me>
2022-08-16 14:09:39 -0500

    fix margin

--- pages/account/[slug].js
+++ pages/account/[slug].js
@@ -5,33 +5,60 @@ import { useContext, useEffect } from "react";
 import { useAuth } from "../../contexts/auth-context";
 import api from "../../contexts/api";
 import { isMobile } from "react-device-detect";
-import { QuestionPreview } from '../../components';
+import { QuestionPreview } from "../../components";
 
 const AccountQuestions = function (props) {
   const { slug } = props;
   return (
-    <Box flex align="center" justify="start" background="#535865" style={{minHeight: 'auto'}}>
-      <Box direction="row" justify="between" style={{minWidth: 700, margin: '1.5em 0 0.5em 0'}}>
-        <Box flex>
-          <Heading level="3">{slug}</Heading>
+    <Box
+      flex
+      align="center"
+      justify="start"
+      background="#535865" 
+      style={{ minHeight: "auto" }}
+    >
+      <Box>
+        <Box
+          direction="row"
+          justify="between"
+          style={{ margin: "1.5em 10px 0.5em 10px" }}
+        >
+          <Box flex>
+            <Heading level="3">{slug}</Heading>
+          </Box>
+          <Box flex align="end" justify="center">
+            {props.questions && (
+              <Text>{props.questions && props.questions.length} Questions</Text>
+            )}
+          </Box>
         </Box>
-        <Box flex align="end" justify="center">
-          {props.questions && (
-            <Text>{props.questions && props.questions.length} Questions</Text>
-          )}
-        </Box>
+        <Box
+          style={{
+            height: "2px",
+            backgroundColor: "#6FFFB0",
+            margin: "0 10px 1em 10px",
+          }}
+        />
+        {props.questions && props.questions.length > 0 ? (
+          <Box flex>
+            {props.questions.map((question) => {
+              return (
+                <QuestionPreview key={question.slug} question={question} />
+              );
+            })}
+          </Box>
+        ) : (
+          <Text>You'll see your questions here</Text>
+        )}
+        <Box
+          style={{
+            minWidth: 700,
+            height: "2px",
+            backgroundColor: "#6FFFB0",
+            margin: "1em 0 1em 0",
+          }}
+        />
       </Box>
-      <Box style={{minWidth: 700, height: '2px', backgroundColor: '#6FFFB0', margin: '0 0 1em 0'}}/>
-      {props.questions && props.questions.length > 0 ? (
-        <Box flex >
-          {props.questions.map((question) => {
-            return <QuestionPreview key={question.slug} question={question} />;
-          })}
-        </Box>
-      ) : (
-        <Text>You'll see your questions here</Text>
-      )}
-      <Box style={{minWidth: 700, height: '2px', backgroundColor: '#6FFFB0', margin: '1em 0 1em 0'}}/>
     </Box>
   );
 };
