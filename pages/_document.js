1feeda139e80c770bbfa3b79e9ec4fe2bb94156c
Sentpiqe <sentpiqe@proton.me>
2022-08-23 18:27:33 -0500

    remove website created with react

--- pages/_document.js
+++ pages/_document.js
@@ -7,10 +7,6 @@ export default function Document() {
         <meta charSet="utf-8" />
         <link rel="icon" href="%PUBLIC_URL%/favicon.ico" />
         <meta name="theme-color" content="#000000" />
-        <meta
-          name="description"
-          content="Web site created using create-react-app"
-        />
         <link rel="apple-touch-icon" href="%PUBLIC_URL%/logo192.png" />
         <link rel="manifest" href="%PUBLIC_URL%/manifest.json" />
         <link
